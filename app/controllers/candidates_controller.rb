3a643481cf735b1514aeb0cdeb08e4f97fbede6e
Eddie Kao <eddie@digik.com.tw>
2017-05-02 16:52:26 +0800

    step06 create candidate

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -6,4 +6,21 @@ class CandidatesController < ApplicationController
   def new
     @candidate = Candidate.new
   end
+
+  def create
+    @candidate = Candidate.new(candidate_params)
+
+    if @candidate.save
+      # 成功
+      redirect_to candidates_path, notice: "新增候選人成功!"
+    else
+      # 失敗
+      render :new
+    end
+  end
+
+  private
+  def candidate_params
+    params.require(:candidate).permit(:name, :age, :party, :politics)
+  end
 end
